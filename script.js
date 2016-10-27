bee7de79128ff09bad123d5e31ffd3d0fd89dfc4
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2016-10-26 19:03:46 -0500

    add object constructor and object instances

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
-
+var State = function(state, revenue, locationCount) {
+ this.state = state;
+ this.revenue = revenue;
+ this.locationCount = locationCount;
+ this.averageRevenue = function() {
+  return (this.revenue / this.locationCount).toFixed(2);
+ };
+};
 // object instances
-
+var illinois = new State("Illinois", 5000, 12);
+var minnesota = new State("Minnesota", 300, 3);
+var nevada = new State("Nevada", 25000, 1);
 // push object instances to locations array
+locations.push(illinois);
+locations.push(minnesota);
+locations.push(nevada);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
